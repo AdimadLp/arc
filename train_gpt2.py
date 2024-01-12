162540625fb5247f5a79c76f2a62771ab08f455f
adimadlp <adimadlp@outlook.com>
2024-01-12 01:38:02 +0100

    Update batch size in train function

--- train_gpt2.py
+++ train_gpt2.py
@@ -39,7 +39,7 @@ class ArcDataset(Dataset):
         return self.data[idx]
 
 def train(model_path, learning_rate, batch_size, epoch):
-    print(f"model_path: {model_path}\nlearning_rate: {learning_rate}\nepoch: {epoch}")
+    print(f"model_path: {model_path}\nlearning_rate: {learning_rate}\nbatch_size{batch_size}\nepoch: {epoch}")
     tokenizer = GPT2Tokenizer.from_pretrained(model_path)
     # Ensure that pad_token is set
     tokenizer.pad_token = tokenizer.eos_token
@@ -122,14 +122,15 @@ if __name__ == '__main__':
         model_path = args.continue_training
         learning_rate = float(model_path.split('_')[-2])
         epoch = int(model_path.split('_')[-1])
+        batch_size = 1
 
-        train(model_path, learning_rate, 1, epoch)
+        train(model_path, learning_rate, batch_size, epoch)
     else:
         model_path = 'gpt2'
         learning_rate = 2e-5
         epoch = 0
-
-        train(model_path, learning_rate, 1, epoch)
+        batch_size = 1
+        train(model_path, learning_rate, batch_size, epoch)
 
     
 """
